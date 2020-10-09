b1b263f097f0e431ec3b1ed870f24353568bd6c9
liyanchao2tytp <57429651+liyanchao2tytp@users.noreply.github.com>
2020-10-09 09:23:56 +0800

    修改了部分文字错误和信任实验结果计算不对的bug等bug

---
+++ pre_public_goods/pages.py
@@ -0,0 +1,48 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Introduction(Page):
+    """Description of the game: How to play and returns expected"""
+
+    pass
+
+
+class Contribute(Page):
+    """Player: Choose how much to contribute"""
+
+    form_model = 'player'
+    form_fields = ['contribution']
+
+    def vars_for_template(self):
+        return dict(
+            round=self.round_number
+
+        )
+
+
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = 'set_payoffs'
+
+    body_text = "Waiting for other participants to contribute."
+
+    def vars_for_template(self):
+        #self.player.player_pay=self.player.player_pay+self.player.contribution
+        return dict(
+
+        )
+
+class Results(Page):
+    """Players payoff: How much each has earned"""
+
+    def vars_for_template(self):
+        #Player.set_app(self)
+        #self.player.set_app(self)
+        # 将用户投入的钱进行保存
+
+        #self.player.player_pay = self.player.contribution+self.player.player_pay
+        return dict(total_earnings=self.group.total_contribution * Constants.multiplier)
+
+
+page_sequence = [Contribute, ResultsWaitPage, Results]
